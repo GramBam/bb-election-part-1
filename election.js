c252c60c40724f07be075101ef6064b41d7228c8
Graham Moss <graham.moss@hotmail.com>
2017-02-17 15:13:19 -0500

    Add candidate loop

--- election.js
+++ election.js
@@ -6,8 +6,15 @@ $(document).ready(function() {
     dataType: 'json'
 
   }).done(function (responseData) {
+
     console.log(responseData);
 
-});
+    for(var i=0; i < responseData.candidates.length; i++) {
+    $('#response-data').append('<li>  Name: ' + responseData.candidates[i].name + '</li>');
+    $('#response-data').append('<li> Votes: ' + responseData.candidates[i].votes + '</li>' + '<br>');
+
+    }
+
+  });
 
 });
